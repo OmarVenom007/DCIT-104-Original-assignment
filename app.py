5708e83f5aaade271bc604bd2f60c8ec2a1f98c8
OmarVenom007 <103182587+OmarVenom007@users.noreply.github.com>
2022-08-07 10:35:02 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+numbers = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+sum_of_numbers = sum(numbers)
+count = len(numbers)
+average = sum_of_numbers / count
+print("The numbers are", numbers)
+print("The average is", average)
+
